0f0f4cbd7c2f9bf1e81747d94235d3e959e05f34
Kaivaan <cariaga.info@gmail.com>
2018-10-17 20:17:12 +0800

    asf

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use((req, res) => res.send("") )
+  .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
@@ -18,7 +18,7 @@ io.on('connection', (socket) => {
   io.emit('count',count);
 
   console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+ // socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-//setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+setInterval(() => io.emit('count',  count++ ), 1000);
