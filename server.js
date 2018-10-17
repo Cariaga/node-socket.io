eff2f3323af947fa9a43bbb5fc925d1ccd85e5d5
Kaivaan <cariaga.info@gmail.com>
2018-10-17 20:10:28 +0800

    asf

--- server.js
+++ server.js
@@ -12,10 +12,13 @@ const server = express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
-
+var count =0;
 io.on('connection', (socket) => {
+  count++;
+  io.emit('count',count);
+
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+//setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
